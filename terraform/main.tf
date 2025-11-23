8d4d59b69671137784a0c65114dcf4d394c68ed0
helnahass <helnahass@ejada.com>
2025-11-23 10:15:36 +0200

    apply fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -319,6 +319,10 @@ resource "aws_iam_user" "github_action_user" {
 resource "aws_iam_user_policy" "github_action_user_permission" {
   user   = aws_iam_user.github_action_user.name
   policy = data.aws_iam_policy_document.github_policy.json
+
+  lifecycle {
+    ignore_changes = [policy]
+  }
 }
 
 data "aws_iam_policy_document" "github_policy" {
